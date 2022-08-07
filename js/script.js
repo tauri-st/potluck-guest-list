1db723d96c0925a2160252f0c10160a2bf34a901
tauri_s <tauristclaire@gmail.com>
2022-08-06 18:02:01 -0700

    Refactor branch merge and set to 8 max then hide input

--- js/script.js
+++ js/script.js
@@ -11,23 +11,39 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+//Click button to add people to list and clear box afterwards
 addGuestButton.addEventListener("click", function () {
 const guest = guestInput.value;
 //console.log(guest);
 
 if (guest !== "") {
-    clearInput();
     addToList(guest);
+    updateGuestCount();
+    clearInput();
 }
 });
 
-//Clear after subbing input then call back function within click event
-const clearInput = function () {
-    guestInput.value = "";
-};
-
+//Create a list element, populate it, add it to the unordered list, add to click event
 const addToList = function (guest) {
     const listItem = document.createElement("li");
     listItem.innerText = guest;
     guestList.append(listItem);
+};
+
+//Limit the Guest List to 8 People
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+//set the innerText of the guestCount variable (the variable that selects the element with the attendance class) to the length of the guests variable then call in click event   
+    guestCount.innerText = guests.length
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+}
+
+//Clear after subbing input then call back function within click event
+const clearInput = function () {
+    guestInput.value = "";
 };
